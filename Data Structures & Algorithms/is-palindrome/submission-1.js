bdc1d298b788552d9d45ec7a217298d34cf210e5
Abdelrahman Mostafa Zaki <abdelrahmanzaki747@gmail.com>
2026-08-13 21:03:27 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+          const cleaned = s
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, "");
+         const reversed = cleaned
+        .split("")
+        .reverse()
+        .join("");
+            return cleaned === reversed;
+    }
+}
