44b934309524c874b8ed3232f957b9440b897e04
Abdelrahman Mostafa Zaki <abdelrahmanzaki747@gmail.com>
2026-09-09 20:02:38 +0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,34 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else {
+            const currentMin = Math.min(
+                val,
+                this.minStack[this.minStack.length - 1]
+            );
+
+            this.minStack.push(currentMin);
+        }
+    }
+
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
