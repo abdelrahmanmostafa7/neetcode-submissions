7c3cf572f3914baf6539853e8fe0252f2ea25408
Abdelrahman Mostafa Zaki <abdelrahmanzaki747@gmail.com>
2026-09-15 19:37:04 +0300

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    reverseList = (head) => {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        const next = current.next;
+
+        current.next = prev;
+
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+};
+}
